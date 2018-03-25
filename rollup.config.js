d79cbf82211ff8b69677c84ebcb5989a31cd217c
Dylan Frankland <dylan@frankland.io>
2018-03-25 16:04:28 -0700

    Prevent caching of Babel results
    Using codegen mucks with the Babel cache. Sometimes the cache will be used, even with new code, giving false-positive tests.

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { dependencies, peerDependencies } from './package.json';
 
+process.env.BABEL_DISABLE_CACHE = 1;
+
 export default {
   input: [
     './src/amphtml/amphtml.js',
