3c47766cd018b49e44ba5c320d43fb8c56bf1797
Dylan Frankland <dylan@frankland.io>
2019-06-02 16:12:41 -0700

    Fix type declaration files

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,3 @@
-declare module 'react-amphtml' {
-  import * as amphtml from './dist/amphtml/amphtml';
+import * as amphtml from './dist/amphtml/amphtml';
 
-  export default amphtml;
-}
-
-declare module 'react-amphtml/helpers' {
-  import { Action, ActionProps, ActionOnProps } from './dist/helpers/Action';
-  import { AmpBindProps, BindProps, Bind } from './dist/helpers/Bind';
-
-  export { Action, ActionProps, ActionOnProps, AmpBindProps, BindProps, Bind };
-}
-
-declare module 'react-amphtml/setup' {
-  import { AmpScripts } from './dist/setup/AmpScripts';
-  import {
-    AmpScriptsManagerContext,
-    AmpScriptsManagerProps,
-    AmpScriptsManager,
-  } from './dist/setup/AmpScriptsManager';
-  import headerBoilerPlate from './dist/setup/headerBoilerplate';
-
-  export {
-    AmpScripts,
-    AmpScriptsManagerContext,
-    AmpScriptsManagerProps,
-    AmpScriptsManager,
-    headerBoilerPlate,
-  };
-}
+export default amphtml;
---
+++ setup.d.ts
@@ -0,0 +1,14 @@
+import AmpScripts from './dist/setup/AmpScripts';
+import AmpScriptsManager, {
+  AmpScriptsManagerContext,
+  AmpScriptsManagerProps,
+} from './dist/setup/AmpScriptsManager';
+import headerBoilerPlate from './dist/setup/headerBoilerplate';
+
+export {
+  AmpScripts,
+  AmpScriptsManagerContext,
+  AmpScriptsManagerProps,
+  AmpScriptsManager,
+  headerBoilerPlate,
+};
