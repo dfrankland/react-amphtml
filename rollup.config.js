6fa6c0c1e9180c629f3556a14eb1fe2b6aa57f4e
Dylan Frankland <dylan@frankland.io>
2019-06-02 13:35:32 -0700

    Add TypeScript declaration export to Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { exec } from 'child_process';
 import { dependencies, peerDependencies } from './package.json';
 
 process.env.BABEL_DISABLE_CACHE = 1;
 
 export default {
+  preserveModules: true,
   input: [
     './src/amphtml/amphtml.tsx',
     './src/helpers/helpers.ts',
@@ -23,6 +25,21 @@ export default {
       extensions: ['.js', '.ts', '.tsx'],
       exclude: 'node_modules/**',
     }),
+    {
+      writeBundle: async () => {
+        await new Promise((resolve, reject) => {
+          exec(
+            'npm run ts-declarations',
+            { cwd: __dirname, env: process.env },
+            (err, stdout, stderr) => {
+              process.stdout.write(stdout);
+              process.stderr.write(stderr);
+              return err ? reject(err) : resolve();
+            },
+          );
+        });
+      },
+    },
   ],
   external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
 };
