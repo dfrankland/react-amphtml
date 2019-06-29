32010db96969d16fb8a0129b33dab32632a3de1b
Dylan Frankland <dylan@frankland.io>
2019-06-29 14:34:54 -0700

    Fix renaming of bundled files

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import { exec } from 'child_process';
-import globby from 'globby';
-import { rename } from 'fs';
+import renameExtensions from '@betit/rollup-plugin-rename-extensions';
 import { dependencies, peerDependencies } from './package.json';
 
 process.env.BABEL_DISABLE_CACHE = 1;
@@ -27,43 +25,13 @@ export default {
       extensions: ['.js', '.ts', '.tsx'],
       exclude: 'node_modules/**',
     }),
-    {
-      writeBundle: async () => {
-        // Rename .ts / .tsx => .js so we don't confuse tsc
-        await Promise.all(
-          (await globby([
-            './dist/**/*.ts',
-            './dist/**/*.tsx',
-            './dist/**/*.ts.map',
-            './dist/**/*.tsx.map',
-          ])).map(
-            file =>
-              new Promise((resolve, reject) => {
-                rename(
-                  file,
-                  file
-                    .replace(/\.tsx?$/, '.js')
-                    .replace(/\.tsx?\.map$/, '.js.map'),
-                  err => (err ? reject(err) : resolve()),
-                );
-              }),
-          ),
-        );
-
-        // Create type declarations
-        await new Promise((resolve, reject) => {
-          exec(
-            'npm run ts-declarations',
-            { cwd: __dirname, env: process.env },
-            (err, stdout, stderr) => {
-              process.stdout.write(stdout);
-              process.stderr.write(stderr);
-              return err ? reject(err) : resolve();
-            },
-          );
-        });
+    renameExtensions({
+      include: ['**/*.ts', '**/*.tsx'],
+      mappings: {
+        '.ts': '.js',
+        '.tsx': '.js',
       },
-    },
+    }),
   ],
   external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
 };
