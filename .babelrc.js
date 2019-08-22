dbe18be854ab8cce26afa0e12b12745317cea0c0
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-08-22 12:45:01 -0300

    exclude regenerator runtime plugin from env preset

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,12 @@
 module.exports = {
   presets: [
     '@babel/preset-typescript',
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        exclude: ['@babel/plugin-transform-regenerator']
+      }
+    ],
     '@babel/preset-react',
   ],
   plugins: [
